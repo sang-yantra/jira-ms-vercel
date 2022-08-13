6f969424f21dc2753d698ffb1fe263dd5dedb127
Anup Mahato <anupmahato033@gmail.com>
2022-08-14 01:01:14 +0530

    added static public file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import path from "path"
+import express from "express"
 import app from "./src/api/api.js"
 
 const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log("Server is running...", PORT));
+app.use('/static', express.static('public'));
