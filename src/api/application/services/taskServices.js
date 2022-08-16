d037f4c7a2d3a4a8f24b006a1f00cd4644f1a819
Anup Mahato <anupmahato033@gmail.com>
2022-08-17 01:54:30 +0530

    added completed, original_estimate in getalltaskresponse

--- src/api/application/services/taskServices.js
+++ src/api/application/services/taskServices.js
@@ -28,6 +28,11 @@ class TaskServices {
         const tasksWithPbi = dbtasks.map(dbtask => {
             const task = { ...TaskInfoDto };
             task.Id = stringify(dbtask.Id);
+            task.Title = dbtask.Title;
+            task.Type = dbtask.Type;
+            task.Status = dbtask.Status;
+            task.Original_Estimate = dbtask.Original_Estimate;
+            task.Completed = dbtask.Completed;
             task.Pbi = dbpbis.filter(dbpbi => stringify(dbpbi.Id) === stringify(dbtask.Pbi_Id))
                 .map(dbpbi => {
                     let pbi = PbiDto;
@@ -40,10 +45,6 @@ class TaskServices {
 
                     return pbi;
                 });
-            task.Title = dbtask.Title;
-            task.Type = dbtask.Type;
-            task.Status = dbtask.Status;
-            task.Remaining = dbtask.Remaining;
 
             return task;
 
