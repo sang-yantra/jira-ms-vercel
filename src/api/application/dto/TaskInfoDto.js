b90b7b5375e2a92005ec751d55cfdf194e09f3d3
pawangithubact <pawan.edu930@gmail.com>
2022-08-20 18:28:52 +0530

    changes in data model

--- src/api/application/dto/TaskInfoDto.js
+++ src/api/application/dto/TaskInfoDto.js
@@ -1,31 +1,8 @@
 import PbiDto from "./PbiDto.js";
-class TaskInfoDtoEx {
-    constructor(Id, Pbi_Id, Title, Type, Description,
-        Status, Acceptance_Criteria, Nfr, Original_Estimate,
-        Remaining, Completed, Priority, Assigned_To, Created_Date, Updated_Date
-    ) {
-
-        [this.Id,
-        this.Pbi_Id,
-        this.Title,
-        this.Type,
-        this.Description,
-        this.Status,
-        this.Acceptance_Criteria,
-        this.Nfr,
-        this.Original_Estimate,
-        this.Remaining,
-        this.Completed,
-        this.Assigned_To,
-        this.Created_Date,
-        this.Updated_Date,
-        ] = arguments
-    }
-}
 
 let TaskInfoDto = {
     Id: String,
-    Pbi: PbiDto,
+    Pbi: PbiDto,    
     Title: String,
     Type: String,
     Description: String,
---
+++ src/api/microservice/controllers/memberController.js
@@ -0,0 +1,9 @@
+/**
+ * members complete list api
+ * members team spcific member 
+ * member for a member id
+ * create member
+ * update member 
+ * update team of a member 
+ * 
+ */
